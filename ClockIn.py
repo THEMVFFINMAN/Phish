0e269fa0046bb191cf5db33122a7ab83650bcc00
JJ <joshuajordanlowe@gmail.com>
2015-07-24 16:32:02 -0600

    Update ClockIn.py

--- ClockIn.py
+++ ClockIn.py
@@ -1,23 +1,31 @@
-# A simple script to track my hours, will probably expand to do monthly totals, etc. 
-
 import re, datetime
 
 fileName = "C:\Users\josh\ownCloud\JJ\Timecard.csv"
 times = ""
 
-
-reg = raw_input('9 AM to 6 PM with an hour lunch? [Y|N]')
-if reg == "Y" or reg == "y":
-    times = ",900,1200,1300,1800,800"
-else: 
-    checkIn = raw_input('Check in: ')
-    lunchBegin = raw_input('Lunch begin: ')
-    lunchEnd = raw_input('Lunch end: ')
-    checkOut = raw_input('Check out: ')
-    total = int(checkOut) - (int(lunchEnd) - int(lunchBegin)) - int(checkIn)
-    times = ",{0},{1},{2},{3},{4}".format(checkIn,lunchBegin,lunchEnd,checkOut,str(total))
+def fix(number):
+    number = int(number) + 0.0
+    return (number-(number%100))/100 + (number%100)/60
 
 def main(fileName):
+    reg = raw_input('9 AM to 6 PM with an hour lunch? [Y|N]')
+    if reg == "Y" or reg == "y":
+        times = ",900,1200,1300,1800,800"
+    else: 
+        checkIn = raw_input('Check in: ')
+        lunchBegin = raw_input('Lunch begin: ')
+        lunchEnd = raw_input('Lunch end: ')
+        checkOut = raw_input('Check out: ')
+        total = fix(checkOut) - (fix(lunchEnd) - fix(lunchBegin)) - fix(checkIn)
+        print fix(checkOut)
+        print fix(lunchEnd)
+        print fix(lunchBegin)
+        print fix(checkIn)
+        print total
+        total = (total - (total % 1)) * 100 + ((total % 1) * 60)
+        print total
+        times = ",{0},{1},{2},{3},{4}".format(checkIn,lunchBegin,lunchEnd,checkOut,str(total))
+    
     with open(fileName, "r") as punchCard:
         text = punchCard.read()
         updateTimeCard = re.sub(',,,,,0', times, text, 1)
